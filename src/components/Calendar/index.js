8bbbbb695d93a17d6a8f1d10ea8dee9923afae4f
Cut Javascript <36466790+cutjavascript@users.noreply.github.com>
2018-06-13 16:37:30 +0530

    add react-sizeme

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'react-awesome-modal';
-
+import Calendar from './components/Calendar';
+import { ViewType } from './components/constant';
 
 class Home extends React.PureComponent {
   constructor(props) {
