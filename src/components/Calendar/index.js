1b989bfa1d1a4f062efd7fa65b7154bb0359dbac
Cut Javascript <36466790+cutjavascript@users.noreply.github.com>
2018-06-14 13:13:19 +0530

    added calendar example files

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,44 +1,139 @@
 import React from 'react';
-import Modal from 'react-awesome-modal';
 import moment from 'moment';
 import {omit, isEqual} from 'lodash';
 import sizeMe from 'react-sizeme';
-// import { ViewType } from './components/constant';
 
+import CalendarHeader from '../header/CalendarHeader';
+import CalendarBody from '../body/CalendarBody';
+import {ViewType} from '../constant';
 
-class Home extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state={visible:false}
-  }
+import '../style/main.scss';
 
-  openModal(){
-    this.setState({
-    visible:true
-    
-    })
-    
-      }
-    
-    closeModal()
-    {
-    this.setState({visible:false});
-    
-    
+import {
+    getBookingsForDay,
+    getBookingsForWeek,
+    getBookingsForMonth,
+    getTimesliceForDay,
+    getTimesliceForWeek,
+    getTimesliceForMonth
+} from '../util';
+
+class Calendar extends React.Component {
+    onDateChangedBound = this.onDateChanged.bind(this);
+    onViewChangedBound = this.onViewChanged.bind(this);
+    onDayChoosenBound = this.onDayChoosen.bind(this);
+
+    static defaultProps = {
+        bookings: [],
+        timeSlices: [],
+        timeSlot: 30,
+        timeExceptions: [],
+        displayPast: false,
+        view: ViewType.Month,
+        date: moment(),
+        resources: {
+            view: {
+                Day: 'Day',
+                Week: 'Week',
+                Month: 'Month'
+            }
+        }
+    };
+
+    constructor(props) {
+        super(props);
+
+        const bookings = props.bookings.map(booking => {
+            booking.startDate = moment.isMoment(booking.startDate) ? booking.startDate : moment(booking.startDate);
+            booking.endDate = moment.isMoment(booking.endDate) ? booking.endDate : moment(booking.endDate)
+            booking.isBooked = true;
+            return booking;
+        });
+
+        this.state = {
+            date: moment(props.date),
+            view: props.view,
+            bookings: bookings,
+            timeSlices: props.timeSlices,
+            timeExceptions: props.timeExceptions,
+            booking: {}
+        }
     }
 
-  render() {
-    return (
-<div>
-<input type="button" value="Book Now" onClick={()=>this.openModal()} />
-<Modal visible={this.state.visible} width="1024" height="900" effect="fadeInUp" onClickAway={()=>this.closeModal()}>
+    componentWillReceiveProps(nextProps) {
+        if (!isEqual(this.state.bookings, nextProps.bookings)) {
+            const bookings = nextProps.bookings.map(booking => {
+                booking.startDate = moment.isMoment(booking.startDate) ? booking.startDate : moment(booking.startDate);
+                booking.endDate = moment.isMoment(booking.endDate) ? booking.endDate : moment(booking.endDate)
+                booking.isBooked = true;
+                return booking;
+            });
 
-</Modal>
-  </div>
+            this.setState({ bookings });
+        }
 
+        if (!isEqual(this.state.timeSlices, nextProps.timeSlices)) {
+            this.setState({ timeSlices: nextProps.timeSlices });
+        }
 
-    );
-  }
+        if (!isEqual(this.state.timeExceptions, nextProps.timeExceptions)) {
+            this.setState({ timeExceptions: nextProps.timeExceptions });
+        }
+    }
+
+    getBookings() {
+        return this.state.view === ViewType.Day
+            ? getBookingsForDay(this.state.bookings, this.state.date)
+            : this.state.view === ViewType.Week
+                ? getBookingsForWeek(this.state.bookings, this.state.date)
+                : getBookingsForMonth(this.state.bookings, this.state.date);
+    }
+
+    getTimetable() {
+        return this.state.view === ViewType.Day
+            ? getTimesliceForDay(this.state.timeSlices, this.state.timeExceptions, this.state.date)
+            : this.state.view === ViewType.Week
+                ? getTimesliceForWeek(this.state.timeSlices, this.state.timeExceptions, this.state.date)
+                : getTimesliceForMonth(this.state.timeSlices, this.state.timeExceptions, this.state.date);
+    }
+
+    render() {
+        return (
+            <div className='rbc-calendar'>
+                <CalendarHeader pastAvailable={this.props.displayPast}
+                    date={this.state.date}
+                    view={this.state.view}
+                    viewChanged={this.onViewChangedBound}
+                    dateChanged={this.onDateChangedBound}
+                    resources={this.props.resources}
+                    size={this.props.size} />
+
+                <CalendarBody bookings={this.getBookings()}
+                    timeSlot={this.props.timeSlot}
+                    timeSlices={this.getTimetable()}
+                    view={this.state.view}
+                    date={this.state.date}
+                    displayPast={this.props.displayPast}
+                    canViewBooking={this.props.canViewBooking}
+                    displayDayView={this.props.displayDayView}
+                    dayClicked={this.onDayChoosenBound}
+                    slotClicked={this.props.onSlotChoosen}
+                    size={this.props.size} />
+            </div>
+        );
+    }
+
+    onDateChanged(date) {
+        this.setState({ date });
+    }
+
+    onViewChanged(view) {
+        this.setState({ view });
+    }
+
+    onDayChoosen(booking) {
+        this.setState({ view: ViewType.Day, date: booking.startDate });
+    }
 }
 
-export default Home;
+export default sizeMe()(Calendar);
