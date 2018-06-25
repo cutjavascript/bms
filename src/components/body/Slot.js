33db6d60cded78692f371ea8f86121a68338ab7b
Cut Javascript <36466790+cutjavascript@users.noreply.github.com>
2018-06-25 20:02:36 +0530

    saved css

--- src/components/body/Slot.js
+++ src/components/body/Slot.js
@@ -43,8 +43,6 @@ export default class Slot extends React.Component {
   }
 
   renderSlot() {
-    console.log("===  Line:35, File:e:gitwork\bmssrccomponents\bodySlot.js");
-
     const sizeTypeModifier = bem("rbc-slot--" + getSizeType(this.props.size));
     const isBookedModifier = bem("rbc-slot", [this.props.isBooked ? "booked" : "free"]);
     const isClickableModifier = bem("rbc-slot", [this.isClickable() ? "clickable" : ""]);
@@ -67,11 +65,8 @@ export default class Slot extends React.Component {
                   {values => {
                     const formatteDate =
                       this.props.startDate && String(moment(this.props.startDate).format("YYYYMMDDhA")).toLowerCase();
-                    console.log("===formatteDate  Line:59, File:e:gitwork\bmssrccomponents\bodySlot.js", formatteDate);
-                    var slotData = _.find(values, { time: formatteDate });
-                    console.log("===slotData  Line:61, File:e:gitwork\bmssrccomponents\bodySlot.js", slotData);
 
-                    console.log("===  Line:38, File:e:gitwork\bmssrccomponents\bodySlot.js", values);
+                    var slotData = _.find(values, { time: formatteDate });
 
                     return this.renderBookingLink(slotData);
                   }}
@@ -85,16 +80,12 @@ export default class Slot extends React.Component {
   }
 
   render() {
-    console.log("===this.props  Line:61, File:e:gitwork\bmssrccomponents\bodySlot.js", this.props);
-    console.log("===this.props.startDate  Line:61, File:e:gitwork\bmssrccomponents\bodySlot.js", this.props.startDate);
     let totalSlots = this.props.totalSlots;
-    console.log("===totalSlots  Line:63, File:e:gitwork\bmssrccomponents\bodySlot.js", totalSlots);
+
     if (this.props.startDate && this.props.startDate.format("HH") && totalSlots && totalSlots.length > 0) {
       let formattedSlotTime = moment(this.props.startDate).format("YYYYMMDDhA");
 
       if (totalSlots.includes(formattedSlotTime.toLowerCase())) {
-        console.log("===formattedSlotTime  Line:105, File:e:gitwork\bmssrccomponentsil.js", formattedSlotTime);
-
         return this.renderSlot();
       }
     }
